4009ff157ed9725cfc2a8de5f96d026dffa72ec3
xala3pa <salazar3pa@gmail.com>
2016-01-19 23:12:49 +0100

    add a service layer

--- grails-app/controllers/qotd/QuoteController.groovy
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -4,17 +4,11 @@ class QuoteController {
 
     static scaffold = true
 
-    def random () {
-        def allQuotes = Quote.list()
-        def randomQuote
+    def quoteService
 
-        if (allQuotes.size() > 0) {
-            def randomIndex = new Random().nextInt(allQuotes.size())
-            randomQuote = allQuotes[randomIndex]
-        } else {
-            randomQuote = new Quote(author: "Anonymous",
-                    content: "Real Programmers Don't eat much Quiche")
-        }
+    def random () {
+        def randomQuote = quoteService.randomQuote
+
         [quote: randomQuote]
     }
 }
